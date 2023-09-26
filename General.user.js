f793a469180c8dfa588339cbde02caa915152e15
Ratithoglys <github.com@ebumna.net>
2023-09-26 17:24:17 +0200

    Update General.user.js
    Add debugging mess

--- General.user.js
+++ General.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Ebumna : BoardGameArena, General
+// @name         BoardGameArena: General
 // @namespace    http://ebumna.net/
-// @version      0.3
+// @version      0.4
 // @description  Misc utils for BoardGameArena
 // @author       Lénaïc JAOUEN
 // @match        https://boardgamearena.com/*
-// @icon         http://en.studio.boardgamearena.com/favicon.ico
+// @icon         http://boardgamearena.com/theme/img/favicon/android-icon-512x512
 // @updateURL    https://raw.githubusercontent.com/Ratithoglys/BGA_Utils/main/General.user.js
 // @downloadURL  https://raw.githubusercontent.com/Ratithoglys/BGA_Utils/main/General.user.js
 // @grant        none
@@ -13,6 +13,13 @@
 
 (function() {
     'use strict';
+
+ 	// Enable for debugging
+	const DEBUG = false;
+	const logDebug = (...msgs) => {
+		// eslint-disable-next-line no-console
+		if (DEBUG) console.log('BGA_GEN> ', msgs);
+	};
 
     const config_stree = {
         childList: true,
@@ -32,12 +39,15 @@
     observer_menu.observe(oRoot, config_stree);
 
     function addGamesButton() {
+        logDebug('addGamesButton()');
         if (document.querySelector('#backToTables') === null) {
             if (/boardgamearena\.com\/\d+\//.test(document.baseURI) && document.querySelector('#upperrightmenu') !== null) {
+                logDebug('addGamesButton() > 🎲 Table > topbar_content');
                 observer_menu.disconnect();
                 document.querySelector('#upperrightmenu').insertAdjacentHTML('afterbegin', '<div class="upperrightmenu_item"><a id="backToTables" class="bgabutton bgabutton_blue globalaction" href="/gameinprogress">🎲</a></div>');
             }
             else if (/boardgamearena\.com\/\d+\//.test(document.baseURI) == false && document.querySelector('.bga-menu-bar-items') !== null) {
+                logDebug('addGamesButton() > 🚫 Table > no #refreshPlayersBtn > bga-menu-bar-items');
                 observer_menu.disconnect();
                 document.querySelector('.bga-menu-bar-items').firstChild.insertAdjacentHTML('afterend','<a id="backToTables" class="bga-menu-bar-items__menu-item bga-link truncate svelte-1duixkh" href="/gameinprogress">🎲 Tables  </a>');
             }
