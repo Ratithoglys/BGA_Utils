faaf4dd3e82066a4acb2c11457d31c76997f4819
Ratithoglys <github.com@ebumna.net>
2023-09-26 11:56:43 +0200

    Update and rename General.js to General.user.js
    Correct urls

--- General.user.js
+++ General.user.js
@@ -6,8 +6,8 @@
 // @author       Lénaïc JAOUEN
 // @match        https://boardgamearena.com/*
 // @icon         http://en.studio.boardgamearena.com/favicon.ico
-// @updateURL    https://raw.githubusercontent.com/Ratithoglys/BGA_Utils/main/Highlight.js
-// @downloadURL  https://raw.githubusercontent.com/Ratithoglys/BGA_Utils/main/Highlight.js
+// @updateURL    https://raw.githubusercontent.com/Ratithoglys/BGA_Utils/main/General.user.js
+// @downloadURL  https://raw.githubusercontent.com/Ratithoglys/BGA_Utils/main/General.user.js
 // @grant        none
 // ==/UserScript==
 
