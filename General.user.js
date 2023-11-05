bd3fc789309fb2eced150012aa7b50f609dc482f
Ratithoglys <github.com@ebumna.net>
2023-11-05 12:02:41 +0100

    Update General.user.js

--- General.user.js
+++ General.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BoardGameArena: General
 // @namespace    http://ebumna.net/
-// @version      0.5
+// @version      0.10
 // @description  Misc utils for BoardGameArena
 // @author       Lénaïc JAOUEN
 // @match        https://boardgamearena.com/*
@@ -10,6 +10,12 @@
 // @downloadURL  https://raw.githubusercontent.com/Ratithoglys/BGA_Utils/main/General.user.js
 // @grant        none
 // ==/UserScript==
+
+    
+// liste des jeux
+//globalUserInfos.game_list.forEach({e,i}=>{if (e.name='martiandice') {console.log('martian dice: '+i)} } );
+
+// TODO : Gestion différenciée des écrans téléphone
 
 (function() {
     'use strict';
@@ -20,6 +26,8 @@
 		// eslint-disable-next-line no-console
 		if (DEBUG) console.log('BGA_GEN> ', msgs);
 	};
+
+    var userColor = null;
 
     document.head.appendChild(document.createElement('style')).innerHTML = `
 #tournament_popup {
@@ -45,7 +53,73 @@
     padding: 8px;
     text-align: center
 }
+
+.ebBox {
+    display: inline-flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 8px;
+    position: fixed;
+    bottom: 24px;
+    right: 2px;
+    z-index: 2;
+}
+.ebBoxBackground {
+    display: flex;
+    padding: 16px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    border-radius: 16px;
+    border: 2px solid rgb(129, 129, 129);
+    background: #FFFAFA;
+    width: 200px;
+}
+.ebBoxLayers {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 8px;
+}
+.ebBoxTitleSectionTwoTextOne {
+    color: rgba(75, 75, 75, 0.8);
+    height: 44px;
+    font-weight: 700;
+    font-size: 16px;
+    margin: 0px;
+    margin-top: -2px;
+    cursor: default;
+}
+.ebBoxTitleSectionTitleText {
+    height: inherit;
+    width: 100%;
+    text-align: center;
+    color: black;
+}
+.ebBoButton {
+    position: relative;
+    display: flex;
+    height: 48px;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    align-self: stretch;
+    font-size: 16px;
+    border-radius: 8px;
+    border: 2px solid rgba(0, 0, 0, 0.20);
+    border-bottom: 4px solid rgba(0, 0, 0, 0.20);
+    background: #007AFF;
+    font-weight: 700;
+    color: #FFF;
+    text-align: center;
+    cursor: pointer;
+    transition: .1s;
 `
+
+    document.body.insertAdjacentHTML('beforeend',`<div id="ebumna-boxes"></div>`);
 
     const config_stree = {
         childList: true,
@@ -59,18 +133,52 @@
         characterData: false,
         subtree: true
     };
+    const config_childs = {
+        childList: true,
+        attributes: false,
+        characterData: false,
+        subtree: false
+    };
 
-    const tournament_popup = '<div id="tournament_popup" style="display:block"><div id="arena_near_end_text" class="roundedboxinner">🏆 This is a Tournament match 🏆</div></div>';
-    const arena_popup = '<div id="arena_popup" style="display:block"><div id="arena_near_end_text" class="roundedboxinner">⚔️ This is an Arena match ⚔️</div></div>';
+    //const tournament_popup = '<div id="tournament_popup" style="display:block"><div id="arena_near_end_text" class="roundedboxinner">🏆 This is a Tournament match 🏆</div></div>';
+    //const arena_popup = '<div id="arena_popup" style="display:block"><div id="arena_near_end_text" class="roundedboxinner">⚔️ This is an Arena match ⚔️</div></div>';
 
-    const oRoot = document.body;
-    const observer_menu = new MutationObserver(addGamesButton);
-    observer_menu.observe(oRoot, config_stree);
+    const observer_menu = new MutationObserver(doShit);
+    observer_menu.observe(document.body, config_stree);
 
-    function addGamesButton() {
+    const observer_popups = new MutationObserver(hideAnnoyingShit);
+    observer_popups.observe(document.body, config_stree);
+
+    const observer_options = new MutationObserver(getOptions);
+    //obeserver_options.observer(document.body, config_stree);
+
+    const observer_color = new MutationObserver(betterPlayerColorNotification);
+    observer_color.observe(document.body, config_childs);
+
+    const observer_banner = new MutationObserver(addAlertsBanners);
+    observer_banner.observe(document.body, config_childs);
+
+    const observer_calendar = new MutationObserver(fixCalendarLinks);
+    observer_calendar.observe(document.body, config_childs);
+
+    const observer_gametables = new MutationObserver(improveGameTables);
+
+    function doShit() {
+        //hideAnnoyingShit();
+        addGamesButtons();
+        //addAlertsBanners();
+
+        console.log('doshit()');
+        if (/boardgamearena\.com\/lobby/.test(document.baseURI) && document.querySelector('#ebBox-GameTableMgr') == null) {
+            console.log('doShit() ui()');
+            gameTables_page_ui();
+        }
+    }
+
+    function addGamesButtons() {
         logDebug('addGamesButton()');
         if (document.querySelector('#backToTables') === null) {
-            if (/boardgamearena\.com\/\d+\//.test(document.baseURI) && document.querySelector('#upperrightmenu') !== null) {
+            if ((/boardgamearena\.com\/\d+\//.test(document.baseURI) || /boardgamearena\.com\/tutorial\?/.test(document.baseURI)) && document.querySelector('#upperrightmenu') !== null) {
                 logDebug('addGamesButton() > 🎲 Table > topbar_content');
                 observer_menu.disconnect();
                 document.querySelector('#upperrightmenu').insertAdjacentHTML('afterbegin', '<div class="upperrightmenu_item"><a id="backToTables" class="bgabutton bgabutton_blue globalaction" href="/gameinprogress">🎲</a></div>');
@@ -84,15 +192,118 @@
         if (document.querySelector('#tournamentsList') === null) {
             if (/boardgamearena\.com\/\d+\//.test(document.baseURI) == false && document.querySelector('.bga-menu-bar-items') !== null) {
                 observer_menu.disconnect();
-                document.querySelector('.bga-menu-bar-items').firstChild.insertAdjacentHTML('afterend','<a id="tournamentsList" class="bga-menu-bar-items__menu-item bga-link truncate svelte-1duixkh" href="/tournamentlist">🏆 Tournaments  </a>');
+                document.querySelector('.bga-menu-bar-items').firstChild.insertAdjacentHTML('afterend','<a id="tournamentsList" class="bga-menu-bar-items__menu-item bga-link truncate svelte-1duixkh" href="/tournamentlist">🏆 Tournois  </a>');
             }
         }
+    }
 
-        if (gameui.tournament_id !== null && document.querySelector('#arena_ending_soon') !== null) {
-            document.querySelector('#arena_ending_soon').insertAdjacentHTML('beforebegin', tournament_popup);
+    function addAlertsBanners() {
+        if (typeof gameui !== 'undefined') {
+            observer_banner.disconnect()
+            if (gameui.tournament_id !== null && document.querySelector('#arena_ending_soon') !== null) {
+                addBox('RankInfo');
+                //document.querySelector('#arena_ending_soon').insertAdjacentHTML('beforebegin', tournament_popup);
+                addBoxTitleLine('RankInfo', '🏆 Tournament match 🏆');
+                document.querySelector('#ebBox-RankInfo > div').style.backgroundColor = 'gold';
+            }
+            else if (document.body.classList.contains('arena_mode') && document.querySelector('#arena_ending_soon') !== null) {
+                addBox('RankInfo');
+                //document.querySelector('#arena_ending_soon').insertAdjacentHTML('beforebegin', arena_popup);
+                addBoxTitleLine('RankInfo', '⚔️ Arena match ⚔️');
+                document.querySelector('#ebBox-RankInfo > div').style.backgroundColor = 'royalblue';
+            }
+            else if (document.body.classList.contains('training_mode') && document.querySelector('#arena_ending_soon') !== null) {
+                addBox('RankInfo');
+                //document.querySelector('#arena_ending_soon').insertAdjacentHTML('beforebegin', arena_popup);
+                addBoxTitleLine('RankInfo', '❤️ Friendly match ❤️');
+                document.querySelector('#ebBox-RankInfo > div').style.backgroundColor = 'lightgrey';
+            }
         }
-        if (document.body.classList.contains('arena_mode') && document.querySelector('#arena_ending_soon') !== null) {
-            document.querySelector('#arena_ending_soon').insertAdjacentHTML('beforebegin', arena_popup);
+    }
+
+    function hideAnnoyingShit() {
+        // Remove the annoying "Personnal notes" popup
+        if (document.querySelector('#turnBasedNotesIncent') !== null) {
+            observer_popups.disconnect();
+            document.querySelector('#turnBasedNotesIncent').innerHTML = null;
         }
+
+        if (document.querySelector('#gamelobby_inner') !== null) {
+            observer_gametables.observe(document.querySelector('#gamelobby_inner') ,config_stree);
+        }
+
+    }
+
+    function gameTables_page_ui() {
+        addBox('GameTableMgr');
+        addBoxBtn('GameTableMgr', 'Show <100 tables', expandBeginnerTables);
+        addBoxBtn('GameTableMgr', 'Hide <100 tables', collapseBeginnerTables);
+    }
+
+    function addBox(bName) {
+        document.querySelector('#ebumna-boxes').insertAdjacentHTML('beforeend','<div id="ebBox-'+bName+'" class="ebBox"><div class="ebBoxBackground"><div class="ebBoxLayers"></div></div></div>');
+    }
+    function addBoxTitleLine(bName, content) {
+        document.querySelector('#ebBox-'+bName+' > .ebBoxBackground > .ebBoxLayers').insertAdjacentHTML('beforeEnd', '<p class="ebBoxTitleSectionTwoTextOne ebBoxTitleSectionTitleText">'+content+'</p>');
+    }
+    function addBoxLine(bName, content) {
+        document.querySelector('#ebBox-'+bName+' > .ebBoxBackground > .ebBoxLayers').insertAdjacentHTML('beforeEnd', '<p class="ebBoxTitleSectionTwoTextOne">'+content+'</p>');
+    }
+    function addBoxBtn(bName, content, clickCallback) {
+        document.querySelector('#ebBox-'+bName+' > .ebBoxBackground > .ebBoxLayers').insertAdjacentHTML('beforeEnd', '<div class="bgabutton bgabutton_blue">'+content+'</p>');
+        document.querySelector('#ebBox-'+bName+' > .ebBoxBackground > .ebBoxLayers > div.bgabutton:last-child').addEventListener('click', clickCallback)
+    }
+
+    function getOptions() {
+    }
+
+    function betterPlayerColorNotification() {
+        if (document.querySelector('#pagemaintitletext') != null) {
+            observer_color.disconnect();
+            userColor = document.querySelector('#player_boards > .current-player-board > div').id.replace('player_board_inner_','');
+
+            document.querySelector('#pagemaintitletext').style.backgroundColor = 'white';
+            document.querySelector('#page-title').style.backgroundColor = '#'+userColor;
+            document.querySelector('#pagemaintitle_wrap').style.backgroundColor = '#'+userColor;
+            //document.querySelector('#board').style.boxShadow = '0px 20px 999px -5px #'+userColor;
+        }
+    }
+
+    function improveGameTables() {
+        observer_gametables.disconnect();
+        document.querySelectorAll('.gamerank_apprentice, .gamerank_beginner').forEach(e => { e.style.backgroundColor = 'pink' });
+        document.querySelectorAll('.how_to_play_button').forEach(e => { e.style.backgroundColor = 'lightgreen' });
+
+        document.querySelectorAll('.table_status_additional').forEach(e => {if (e.innerText == '(Friendly mode)') { e.closest('.gametable').style.backgroundColor = 'lightgrey' } })
+        if (document.querySelector('#gamelobby_inner') !== null) {
+            observer_gametables.observe(document.querySelector('#gamelobby_inner') ,config_stree);
+        }
+
+    }
+
+    function fixCalendarLinks() {
+        if (document.querySelector('#calendarDlg > p > a') == null) {
+            return
+        }
+
+        observer_calendar.disconnect();
+        if (document.querySelector('#calendarDlg > p > a').attributes.length > 3) {
+            let start = /href="/.exec(document.querySelector('#calendarDlg > p > a').outerHTML).index + 6;
+            let end = /" class="/.exec(document.querySelector('#calendarDlg > p > a').outerHTML).index - 1;
+            let fixedLink = /href=\"(.*)\" /.exec(document.querySelector('#calendarDlg > p > a').outerHTML)[1].replace(/ /g,'').replace(/"/g,'');
+
+            document.querySelector('#calendarDlg > p > a').outerHTML = document.querySelector('#calendarDlg > p > a').outerHTML.replace(document.querySelector('#calendarDlg > p > a').outerHTML.substring(start, end), /href=\"(.*)\" /.exec(document.querySelector('#calendarDlg > p > a').outerHTML)[1].replace(/ /g,'').replace(/"/g,''));
+            document.querySelector('#calendarDlg > p > a').setAttribute('href', document.querySelector('#calendarDlg > p > a').getAttribute('href').replace(/&text=/,'&text=🎲%20'));
+        }
+        else {
+            document.querySelector('#calendarDlg > p > a').setAttribute('href', document.querySelector('#calendarDlg > p > a').getAttribute('href').replace(/&text=/,'&text=🎲%20'));
+        }
+    }
+
+    function expandBeginnerTables() {
+        document.querySelectorAll('#favorite_games_list .gamerank_apprentice, #favorite_games_list .gamerank_beginner').forEach(e => { e.closest('.wannaplay').querySelector('.game_box_image_wrap .game_box').click(); });
+    }
+    function collapseBeginnerTables() {
+        document.querySelectorAll('#favorite_expanded .gamerank_apprentice, #favorite_expanded .gamerank_beginner').forEach(e => { e.closest('.game_box_wrap').querySelector('.game_box').click(); });
     }
 })();
