18cd4aa765ee434ad251d6f998227089843f6bcd
Ratithoglys <github.com@ebumna.net>
2023-11-15 22:50:42 +0100

    Create sechsnimmt.user.js

---
+++ sechsnimmt.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         BoardGameArena: Games: 6 nimmt!
+// @namespace    https://ebumna.net/
+// @version      0.1
+// @description  BoardGameArena: Games: 6 nimmt!
+// @author       Lénaïc JAOUEN
+// @match        https://boardgamearena.com/*/sechsnimmt?table=*
+// @icon         http://boardgamearena.com/theme/img/favicon/android-icon-512x512
+// @updateURL    https://raw.githubusercontent.com/Ratithoglys/BGA_Utils/main/sechsnimmt.user.js
+// @downloadURL  https://raw.githubusercontent.com/Ratithoglys/BGA_Utils/main/sechsnimmt.user.js
+// @grant        none
+// ==/UserScript==
+
+// TODO : Mise en évidence de la règle
+
+(function() {
+    'use strict';
+
+ 	// Enable for debugging
+	const DEBUG = false;
+	const logDebug = (...msgs) => {
+		// eslint-disable-next-line no-console
+		if (DEBUG) console.log('BGA_GEN> ', msgs);
+	};
+
+    const config_childs = {
+        childList: true,
+        attributes: false,
+        characterData: false,
+        subtree: false
+    };
+
+    const observer_banner = new MutationObserver(addOptionBanner);
+    observer_banner.observe(document.body, config_childs);
+
+    function addOptionBanner() {
+        if (document.querySelector('#pagemaintitletext') != null) {
+            observer_banner.disconnect();
+
+            let game_mode = document.querySelector('#footer_option_value_101').innerText;
+
+            if (game_mode.match(/professionnal/)) {
+                document.querySelector('#active_player_statusbar').insertAdjacentHTML('beforebegin','⬅️➡️');
+            }
+            else {
+                document.querySelector('#active_player_statusbar').insertAdjacentHTML('beforebegin','➡️');
+            }
+        }
+    }
+
+})();
